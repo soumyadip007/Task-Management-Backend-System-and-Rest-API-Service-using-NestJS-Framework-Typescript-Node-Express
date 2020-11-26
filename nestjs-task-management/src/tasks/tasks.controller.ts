7d9efe1c938624ff200cc5b4ec49cd8f3bc57a17
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-26 12:07:33 +0530

    Nest JS project Env Setup

---
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -0,0 +1,9 @@
+import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+
+@Controller()
+export class TasksController {
+
+    constructor(private tasksService: TasksService){}
+
+}
